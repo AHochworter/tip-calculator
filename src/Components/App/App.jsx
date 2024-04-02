3147023fc112bc73839bef32e7dac070ee754ca6
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-04-02 17:06:40 -0400

    Add JSX structure for header, calculator, tip, and bill

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react';
-import './App.css';
-import Bill from '../Bill/Bill';
-import Tip from '../Tip/Tip';
+import { useState } from "react";
+import "./App.css";
+import Bill from "../Bill/Bill";
+import Tip from "../Tip/Tip";
 
 function App() {
   return (
-    <>
-      <Bill />
-      <Tip />
-    </>
+    <div className="app">
+      <header className='header'>
+        <img src='src/assets/images/logo.svg' alt='splitter logo' />
+      </header>
+      <div className='calculator-wrapper'>
+        <div className="bill-wrapper">
+        <Bill />
+        </div>
+        <div className="tip-wrapper">
+        <Tip />
+        </div>
+      </div>
+    </div>
   );
 }
 
