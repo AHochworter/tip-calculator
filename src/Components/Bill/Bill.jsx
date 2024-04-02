d847ec62c7e841747c0913d0d4ef6a25614617e6
Ann Hochworter <ann.hochworter@gmail.com>
2024-04-02 14:14:39 -0600

    Add Tip and Bill components to App.jsx

--- src/Components/Bill/Bill.jsx
+++ src/Components/Bill/Bill.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './Bill.css';
 
 function Bill() {
-  return <div>Bill</div>;
+  return (
+    <div>
+      <h1>BILL ON THIS SIDE</h1>
+    </div>
+  );
 }
 
 export default Bill;
--- src/Components/Tip/Tip.jsx
+++ src/Components/Tip/Tip.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './Tip.css';
 
 function Tip() {
-  return <div>Tip</div>;
+  return (
+    <div>
+      <h1>TIP GOES HERE!</h1>
+    </div>
+  );
 }
 
 export default Tip;
