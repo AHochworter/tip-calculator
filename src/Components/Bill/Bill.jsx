ff13622519e7998da2401456882195d0fe7de1fc
Ann Hochworter <ann.hochworter@gmail.com>
2024-04-02 21:33:08 -0600

    Continue styling the Bill side of our calculator

--- src/Components/Bill/Bill.jsx
+++ src/Components/Bill/Bill.jsx
@@ -3,45 +3,63 @@ import './Bill.css';
 
 function Bill() {
   return (
-    <form className='bill-input-wrapper'>
-      <label for="bill" className='bill-label'>Bill</label>
-        <input 
-        type="text" 
-        id="bill" 
-        name="bill" 
+    <form className="bill-input-wrapper">
+      <label htmlFor="bill" className="bill-label">
+        Bill
+      </label>
+      <input
+        type="text"
+        id="bill"
+        name="bill"
         // value={}
+      />
+      <div className="tip-percent-wrapper">
+        <label htmlFor="tip-percent" className="tip-percent-label">
+          Select Tip %
+        </label>
+      </div>
+      <div className="button-wrapper">
+        <input type="button" className="button" name="tip-percent" value="5%" />
+        <input
+          type="button"
+          className="button"
+          name="tip-percent"
+          value="10%"
         />
-        <label for="tip-percent" className='tip-percent-label'>Select Tip %</label>
-        <input 
-        type='button'
-        name='tip-percent'
-        value="5%"
+        <input
+          type="button"
+          className="button"
+          name="tip-percent"
+          value="15%"
         />
-        <input 
-        type='button'
-        name='tip-percent'
-        value="10%"
+        <input
+          type="button"
+          className="button"
+          name="tip-percent"
+          value="25%"
         />
-        <input 
-        type='button'
-        name='tip-percent'
-        value="15%"
+        <input
+          type="button"
+          className="button"
+          name="tip-percent"
+          value="50%"
         />
-        <input 
-        type='button'
-        name='tip-percent'
-        value="25%"
+        <input
+          type="button"
+          className="button"
+          name="tip-percent"
+          value="Custom"
         />
-        <input 
-        type='button'
-        name='tip-percent'
-        value="50%"
-        />
-        <input 
-        type='button'
-        name='tip-percent'
-        value="Custom"
-        />
+      </div>
+      <label htmlFor="number-of-people" className="num-people-label">
+        Number of People
+      </label>
+      <input
+        type="text"
+        id="num-of-people"
+        name="number-of-people"
+        // value={}
+      />
     </form>
   );
 }
